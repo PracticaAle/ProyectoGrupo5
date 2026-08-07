c7258aabe9fac335e9132db8903ec717efed24f2
Dylan <dylan.vargas.dev@gmail.com>
2026-08-07 16:04:51 -0600

    Update Sede.java
    Actualización clase sede y javadoc en métodos

--- ProyectoMundial_Modulo2/ProyectoMundial_Modulo2/src/main/java/com/mycompany/proyectomundial/Sede.java
+++ ProyectoMundial_Modulo2/ProyectoMundial_Modulo2/src/main/java/com/mycompany/proyectomundial/Sede.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyectomundial;
 
 import javax.swing.JOptionPane;
@@ -11,12 +7,13 @@ import javax.swing.JOptionPane;
  * @author Dylan
  */
 public class Sede {
+
     private String nombre;
     private String ciudad;
     private int capacidad;
     private double precioEntrada;
 
-    //Creación del constructor.
+    // Constructor
     public Sede(String nombre, String ciudad, int capacidad, double precioEntrada) {
         this.nombre = nombre;
         this.ciudad = ciudad;
@@ -24,7 +21,7 @@ public class Sede {
         this.precioEntrada = precioEntrada;
     }
 
-    //Gets y Sets.
+    // Gets y Sets
     public String getNombre() {
         return nombre;
     }
@@ -56,13 +53,17 @@ public class Sede {
     public void setPrecioEntrada(double precioEntrada) {
         this.precioEntrada = precioEntrada;
     }
-    
-    //Método para añadir Equipos.
+
+    /**
+     * Metodo para agregar una sede.
+     * @param sedes 
+     */
     public static void agregarSede(Sede[] sedes) {
-        //Buscar la primera posición libre
+
         int posicion = -1;
 
         for (int i = 0; i < sedes.length; i++) {
+
             if (sedes[i] == null) {
                 posicion = i;
                 break;
@@ -70,33 +71,45 @@ public class Sede {
         }
 
         if (posicion == -1) {
+
             JOptionPane.showMessageDialog(null, "No hay espacio para más sedes.");
             return;
         }
 
-        //Pedir datos
-        String nombre = JOptionPane.showInputDialog("Nombre de la sede: ");
-        String ciudad = JOptionPane.showInputDialog("Ciudad donde se ubica la sede: ");
-        int capacidad = Integer.parseInt(JOptionPane.showInputDialog("Capacidad: "));
-        double precioEntrada = Double.parseDouble(JOptionPane.showInputDialog("Precio por entrada: "));
+        String nombre = JOptionPane.showInputDialog("Nombre de la sede:");
 
-        //Crear el objeto
+        String ciudad = JOptionPane.showInputDialog("Ciudad donde se ubica la sede:");
+
+        int capacidad = Integer.parseInt(JOptionPane.showInputDialog("Capacidad:"));
+
+        double precioEntrada = Double.parseDouble(JOptionPane.showInputDialog("Precio por entrada:"));
+
         sedes[posicion] = new Sede(nombre, ciudad, capacidad, precioEntrada);
 
         JOptionPane.showMessageDialog(null, "Sede agregada correctamente.");
     }
-    
-    //Método para mostrar Sedes.
+
+    /**
+     * Metodo para mostrar las sedes registradas
+     * @param sedes 
+     */
     public static void mostrarSedes(Sede[] sedes) {
         String mensaje = "";
 
         for (int i = 0; i < sedes.length; i++) {
+
             if (sedes[i] != null) {
+
                 mensaje += "Sede #" + (i + 1) + "\n";
+
                 mensaje += "Nombre: " + sedes[i].getNombre() + "\n";
-                mensaje += "Ciudad: " + sedes[i].getCiudad()+ "\n";
-                mensaje += "Capacidad: " + sedes[i].getCapacidad()+ "\n";
-                mensaje += "Precio por Entrada: " + sedes[i].getPrecioEntrada()+ "\n";
+
+                mensaje += "Ciudad: " + sedes[i].getCiudad() + "\n";
+
+                mensaje += "Capacidad: " + sedes[i].getCapacidad() + "\n";
+
+                mensaje += "Precio por Entrada: " + sedes[i].getPrecioEntrada() + "\n";
+
                 mensaje += "----------------------------\n";
             }
         }
@@ -107,8 +120,11 @@ public class Sede {
 
         JOptionPane.showMessageDialog(null, mensaje);
     }
-    
-    //Método para actualizar Sedes.
+
+    /**
+     * Metodo para actualizar los datos de una sede previamente registrada.
+     * @param sedes 
+     */
     public static void actualizarSedes(Sede[] sedes) {
 
         String nombrePorBuscar = JOptionPane.showInputDialog("Digite el nombre de la sede que desea actualizar:");
@@ -117,13 +133,15 @@ public class Sede {
 
         for (int i = 0; i < sedes.length; i++) {
 
-            if (sedes[i] != null &&
-                nombrePorBuscar.equalsIgnoreCase(sedes[i].getNombre())) {
+            if (sedes[i] != null && nombrePorBuscar.equalsIgnoreCase(sedes[i].getNombre())) {
 
                 String nombre = JOptionPane.showInputDialog("Nuevo nombre de la sede:");
+
                 String ciudad = JOptionPane.showInputDialog("Nueva ciudad:");
-                int capacidad = Integer.parseInt(JOptionPane.showInputDialog("Capacidad: "));
-                double precioEntrada = Double.parseDouble(JOptionPane.showInputDialog("Precio por entrada: "));
+
+                int capacidad = Integer.parseInt(JOptionPane.showInputDialog("Capacidad:"));
+
+                double precioEntrada = Double.parseDouble(JOptionPane.showInputDialog("Precio por entrada:"));
 
                 sedes[i].setNombre(nombre);
                 sedes[i].setCiudad(ciudad);
@@ -138,12 +156,15 @@ public class Sede {
         }
 
         if (!encontrado) {
-            JOptionPane.showMessageDialog(null,
-                    "La sede no se encuentra registrada.");
+
+            JOptionPane.showMessageDialog(null, "La sede no se encuentra registrada.");
         }
     }
-    
-    //Método de Modo Demo para completar la lista del objeto Sede.
+
+    /**
+     * Metodo para generar todas las sedes con valores aleatorios - Modo Demo.
+     * @param sedes 
+     */
     public static void modoDemoSedes(Sede[] sedes) {
 
         for (int i = 0; i < sedes.length; i++) {
@@ -151,11 +172,14 @@ public class Sede {
             if (sedes[i] == null) {
 
                 String nombre = "Sede #" + (i + 1);
-                String ciudadad = "Sede #" + (i + 1);
-                int capacidad = ((int)(Math.random() * 50) + 1);
-                double precioEntrada = ((double)(Math.random() * 50) + 1);
 
-                sedes[i] = new Sede(nombre, ciudadad, capacidad, precioEntrada);
+                String ciudad = "Ciudad #" + (i + 1);
+
+                int capacidad = ((int) (Math.random() * 50000) + 10000);
+
+                double precioEntrada = ((double) (Math.random() * 50) + 1);
+
+                sedes[i] = new Sede(nombre, ciudad, capacidad,  precioEntrada);
             }
         }
     }
